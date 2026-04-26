4a196234254138336512b73f695a42de4b1a0737
abusufiyansaava <abusufiyansaava@gmail.com>
2026-04-26 19:16:29 +0300

    Fix: Stripe lib - remove apiVersion to avoid type mismatch

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,8 @@
 // src/lib/stripe.ts
-import Stripe from 'stripe'
+//import Stripe from 'stripe'
 
 // 🔑 Server-side only. Never exposed to browser.
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
-  typescript: true,
-})
+//export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+ // apiVersion: '2024-06-20',
+ // typescript: true,
+//})
